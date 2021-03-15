1202a2472f478115e248e762b27fe0a128a0471b
CarlosAE <amiranda@ae-energiasolar.com>
2021-03-15 14:28:22 -0600

    Actualizacion

--- app/Http/Controllers/reportes/AsistenciaRPTController.php
+++ app/Http/Controllers/reportes/AsistenciaRPTController.php
@@ -2,7 +2,7 @@
 
 namespace App\Http\Controllers\reportes;
 
-use App\Exports\AsistenciaExport;
+use App\exports\AsistenciaExport;
 use App\Http\Controllers\Controller;
 use App\Models\empleados;
 use App\Models\marcacionesempleados;
